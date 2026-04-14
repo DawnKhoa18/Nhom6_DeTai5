4d1322ed9658bab63dc23632ad062141db112a0d
nguyenhung1204 <nguyenminhhungh2o@gmail.com>
2026-04-14 14:24:25 +0700

    add chitiet don hang

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'screens/device_detail_screen.dart';
 import 'screens/rental_orders_screen.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: RentalOrdersScreen(),
+      home: DeviceDetailScreen(),
     );
   }
 }
